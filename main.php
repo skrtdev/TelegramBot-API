8f0a11a1325d46c76bc1fca9827e33ed8ab35c17
skrtdev <64538010+skrtdev@users.noreply.github.com>
2020-05-25 19:41:06 +0200

    test

--- main.php
+++ main.php
@@ -1,8 +1,71 @@
-<?
+<?php
 
 class TelegramBot {
-    public function __construct($token) {
+    public function __construct(string $token) {
         $this->token = $token;
+        $this->update = json_decode(file_get_contents("php://input"), true);
+        $this->update = json_decode('{
+            "update_id": 143957909,
+            "message": {
+                "message_id": 210450,
+                "from": {
+                    "id": 634408248,
+                    "is_bot": false,
+                    "first_name": "⁣",
+                    "last_name": "𝓖 Ðᴇᴠ ✓✰",
+                    "username": "gaetano555",
+                    "language_code": "it"
+                },
+                "chat": {
+                    "id": 634408248,
+                    "first_name": "⁣",
+                    "last_name": "𝓖 Ðᴇᴠ ✓✰",
+                    "username": "gaetano555",
+                    "type": "private"
+                },
+                "date": 1590425893,
+                "text": "start?"
+            }
+        }', true);
+
+        $this->update = $this->JSONToTelegramObject($this->update, "update");
+
+        $this->json = json_decode(implode(file("json.json")), true);
+    }
+
+
+    private function getObjectType(string $parameter_name){
+        $this->json = json_decode(implode(file("json.json")), true);
+        echo "getObjectType $parameter_name";
+        var_dump($this->json['available_types'][$parameter_name]);
+        //var_dump($this->json);
+        return isset($this->json['available_types'][$parameter_name]) ? $this->json['available_types'][$parameter_name] : false;
+        return $this->json[$parameter_name];
+
+    }
+    private function JSONToTelegramObject(array $json, string $parameter_name){
+        echo "JSONToTelegramObject\n";
+        foreach($json as $key => $value){
+            echo "$key => $value \n";
+            $valuetype = gettype($value);
+
+
+            if($valuetype === "array"){
+                if($this->getObjectType($key)){
+                    $json[$key] = $this->JSONToTelegramObject($value, $this->getObjectType($key));
+                }
+            }
+        }
+
+        return new TelegramObject($this->getObjectType($parameter_name), $json);
+    }
+}
+
+class TelegramObject extends TelegramBot{
+    public function __construct(string $type, array $json){
+        $this->type = $type;
+        $this->json = $json;
+        return $this;
     }
 }
 
